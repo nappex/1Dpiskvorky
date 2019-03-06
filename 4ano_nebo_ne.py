cffdcf5b927a7b9820d2ea0f474570ab7c2ec1d4
David Stroch <d.stroch@gmail.com>
2019-03-06 13:54:37 +0100

    Program ano nebo ne problem s mezery se vyresil pomoci metody strip. nyni j treba vyresit pr ona vstup ne nereaguje program spravne.

---
+++ 4ano_nebo_ne.py
@@ -0,0 +1,17 @@
+def ano_nebo_ne(otazka):
+    "Vrátí True nebo False, podle odpovědi uživatele"
+    while True:
+        odpoved = input(otazka)
+        odpoved = odpoved.lower().strip()
+        if odpoved == "a" or "ano":
+            return True
+        elif odpoved == "n" or "ne":
+            return False
+        else:
+            print('Nerozumím! Odpověz "ano/ne" nebo "a/n".')
+
+
+if ano_nebo_ne('Chceš si zahrát hru? '):
+    print('OK! Ale napřed si ji musíš naprogramovat.')
+else:
+    print('Škoda.')
